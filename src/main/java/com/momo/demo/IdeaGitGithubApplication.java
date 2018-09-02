307b3494a11c88b5615dcd6b32ed61c5eeb45f79
global <754441650@qq.com>
2018-09-02 12:36:47 +0800

    使用idea测试git提交

---
+++ src/main/java/com/momo/demo/IdeaGitGithubApplication.java
@@ -0,0 +1,12 @@
+package com.momo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaGitGithubApplication.class, args);
+	}
+}
